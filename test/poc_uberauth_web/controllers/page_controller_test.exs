06bc76b30fdc769186bdb915e0d3af90d9cc6d45
bryan <bryan.hunt@erlang-solutions.com>
2018-10-16 17:12:20 +0100

    fix test

--- test/poc_uberauth_web/controllers/page_controller_test.exs
+++ test/poc_uberauth_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PocUberauthWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Don't forget to login !"
   end
 end
